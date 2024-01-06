5609f52368c2b53703324f1252b5ad7639cfda12
Jinhyeong <daegartwo@gmail.com>
2024-01-06 23:22:09 +0900

    comment

---
+++ init-firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDaPAoKE6DOre7MuSCCofHjxrcz1RaTUic",
+    authDomain: "invisibletimer-e7aa3.firebaseapp.com",
+    projectId: "invisibletimer-e7aa3",
+    storageBucket: "invisibletimer-e7aa3.appspot.com",
+    messagingSenderId: "982191168166",
+    appId: "1:982191168166:web:05ff097cb02bded021b267",
+    measurementId: "G-F8X6TRRYNM"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);

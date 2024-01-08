e138020602e87fff46b144d83dd1bb5b88cb06ac
Jinhyeong <daegartwo@gmail.com>
2024-01-08 19:58:37 +0900

    update

--- youtube/youtube.py
+++ youtube/youtube.py
@@ -1,9 +1,9 @@
 import googleapiclient.discovery
 import googleapiclient.errors
 from googleapiclient.errors import HttpError
-from flask import Flask, jsonify, send_from_directory
+from flask import Flask, jsonify, request
 from flask_cors import CORS
-import logging
+import random
 
 api_service_name = 'youtube'
 api_version = "v3"
@@ -13,31 +13,35 @@ youtube = googleapiclient.discovery.build(
     api_service_name, api_version, developerKey=DEVELOPER_KEY
 )
     
-def get_top_n_videos(n, region_code='KR'):
+def get_top_n_videos(numVidoes, region_code='KR'):
     request = youtube.videos().list(
         part='snippet',
         chart='mostPopular',
         regionCode=region_code,
-        maxResults=n
+        maxResults=100
     )
 
     response = request.execute()
-
+    print(len(response['items']))
     results = []
-    
-    for item in response['items']:
+    randomList = random.sample(range(0, 100), numVidoes)
+    print(randomList)
+    for idx, item in enumerate(response['items']):
+        if idx not in randomList:
+            continue
         snippet = item['snippet']
 
+        print(snippet['title'])
         results.append({"videoId": item['id'], "title": snippet['title'], "channelTitle": snippet['channelTitle'],
                         "publishedDate": snippet['publishedAt'], "tags": snippet['tags'] if snippet.get('tags') is not None else []})
     return results
 
-def get_top_n_comments(video_id, n):
+def get_top_n_comments(video_id, numComments):
     try:
         request = youtube.commentThreads().list(
             part="snippet",
             videoId=video_id,
-            maxResults=n
+            maxResults=numComments
         )
         response = request.execute()
 
@@ -49,37 +53,39 @@ def get_top_n_comments(video_id, n):
     except HttpError as e:
         return []
 
-def get_top_n_contents(numVideos=5, numComments=15, region_code='US'):
+def get_top_n_contents(numVideos, numComments, region_code='US'):
     topNvideos = get_top_n_videos(numVideos, region_code)
     results = []
     for video in topNvideos:
         videoId = video['videoId']
         topNcomments = get_top_n_comments(videoId, numComments)
-        result = {}
-        result['title'] = video['title']
-        result['channelTitle'] = video['channelTitle']
-        result['tags'] = video['tags']
-        result['comments'] = topNcomments
-        results.append(result)
+        results.append({
+            'title': video['title'],
+            'channelTitle': video['channelTitle'],
+            'tags': video['tags'],
+            'comments': topNcomments,
+            'hyperlink': "https://www.youtube.com/watch?v=" + videoId
+        })
     return results
 
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/', methods=['GET'])
+def response():
+    # a = request.args.get('key1')
+    # print(a)
+    top5contents = get_top_n_contents(numVideos=10, numComments=15, region_code='KR')
+    return jsonify(top5contents)
 
 
+# CORS(app)
+# @app.route('/test', methods=['POST'])
+# def respons():
+#     a = request.get_json()
+#     print("===============================")
+#     print(a['message'])
+#     print("===============================")
+#     return jsonify("here is response!")
 
-    
-# @app.route('/<path:filename>')
-# def static_files(filename):
-#     return send_from_directory('.', filename)
-
-if __name__ == '__main__':
-    app = Flask(__name__)
-    logging.basicConfig(level=logging.DEBUG)
-    CORS(app)
-
-    @app.route('/', methods=['GET'])
-    def response():
-        top5contents = get_top_n_contents(region_code='KR')
-        app.logger.info("dd")
-        return jsonify(top5contents)
-    
-    app.run(debug=True)
+app.run(debug=True)
